0d64bd699a76f431cf55c99c95a44add14b5a8cb
justinehg <justinehoang21@gmail.com>
2022-07-27 00:29:57 -0500

    update files

--- server.js
+++ server.js
@@ -1,25 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
 const app = express();
-
-
+const MongoClient = require('mongodb').MongoClient
 
 MongoClient.connect('mongodb+srv://jhoang:Quynh2001@cluster0.mcjk6.mongodb.net/?retryWrites=true&w=majority', {
     useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to Database')
         const db = client.db('star-wars-quotes')
+        const quotesCollection = db.collection('quotes')
+
+        app.set('view engine', 'ejs')
         app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(bodyParser.json())
+        app.use(express.static('public'))
+
         app.listen(3000, function() {
             console.log('listening on 3000')
         })
         app.get('/', (req, res) => {
-            res.sendFile(__dirname + '/index.html')
+            const cursor = db.collection('quotes').find()
+            console.log(cursor)
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    res.render('index.ejs', { quotes: results })
+            })
+                 .catch(error => console.error(error))
         })
         app.post('/quotes', (req, res) => {
-            console.log(req.body)
+            quotesCollection.insertOne(req.body)
+              .then(result => {
+                res.redirect('/')
+              })
+              .catch(error => console.error(error))
         })
+        app.put('/quotes', (req, res) => {
+            console.log(req.body)
+          })
     })
-    .catch(console.error)
+    
 
